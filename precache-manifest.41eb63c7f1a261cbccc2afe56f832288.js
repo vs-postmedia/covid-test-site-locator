22c070b75c14929b2d04d8aea651867f623851f3
Nathan Griffiths <ngriffiths@postmedia.com>
2020-08-21 11:14:36 -0700

    Updates

---
+++ precache-manifest.41eb63c7f1a261cbccc2afe56f832288.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c64793a818732d706e8bb560f4377a4",
+    "url": "/covid-test-site-locator/index.html"
+  },
+  {
+    "revision": "ea36459d6438d7ed7232",
+    "url": "/covid-test-site-locator/static/css/main.b92d2884.chunk.css"
+  },
+  {
+    "revision": "357e423946d27b0c43da",
+    "url": "/covid-test-site-locator/static/js/2.f8e5cc5d.chunk.js"
+  },
+  {
+    "revision": "607f4dba556022527177500adaa9aa68",
+    "url": "/covid-test-site-locator/static/js/2.f8e5cc5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea36459d6438d7ed7232",
+    "url": "/covid-test-site-locator/static/js/main.6b786b3e.chunk.js"
+  },
+  {
+    "revision": "6c640a58d31e41b35d94",
+    "url": "/covid-test-site-locator/static/js/runtime-main.a506f206.js"
+  },
+  {
+    "revision": "6f6753fd733a216b24786a168657540a",
+    "url": "/covid-test-site-locator/static/media/BentonSansCond-Bold.6f6753fd.otf"
+  },
+  {
+    "revision": "a6640d8951e3d6c523b2b58b0db9e351",
+    "url": "/covid-test-site-locator/static/media/BentonSansCond-BoldItalic.a6640d89.otf"
+  },
+  {
+    "revision": "69a4217c7da7462a2511b1a8ecaefecf",
+    "url": "/covid-test-site-locator/static/media/BentonSansCond-RegItalic.69a4217c.otf"
+  },
+  {
+    "revision": "43cd4ac75abf53e257d21366d3322b0e",
+    "url": "/covid-test-site-locator/static/media/BentonSansCond-Regular.43cd4ac7.otf"
+  },
+  {
+    "revision": "cdd38a860699d029f95e76f21e51e216",
+    "url": "/covid-test-site-locator/static/media/Shift-Bold.cdd38a86.otf"
+  },
+  {
+    "revision": "5c3edbfdeedc59bb35fac3bf1a461926",
+    "url": "/covid-test-site-locator/static/media/Shift-BoldItalic.5c3edbfd.otf"
+  }
+]);
